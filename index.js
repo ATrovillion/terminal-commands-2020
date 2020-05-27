f23cf44e9c5a6b2340eccc9144771f1ef9be1b34
Aaron Trovillion <aaron.trovillion@gmail.com>
2020-05-27 10:41:43 -0400

    Add completed ' working with node modules' assignment work

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
+const readLine = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 const terminalCommands = require('./terminal-commands');
 
-terminalCommands.ls();
-terminalCommands.touch();
-terminalCommands.mkdir();
+readLine.question('Which command would you like to use? ', name => {
+  if (name === 'ls') {
+    terminalCommands.ls();
+  } else if (name === 'mkdir') {
+    readLine.question('What is the name of the new directory? ', dirName => {
+      terminalCommands.mkdir(dirName);
+    });
+  } else if (name === 'touch') {
+    readLine.question('What is the name of the new file? ', fileName => {
+      terminalCommands.touch(fileName);
+    });
+  }
+});
--- terminal-commands.js
+++ terminal-commands.js
@@ -5,15 +5,27 @@ module.exports.ls = () => {
     const filesToString = files.reduce((acc, file) => `${acc} ${file}`, '');
 
     console.log(filesToString);
+    process.exit(0);
   });
 };
 
-module.exports.touch = name => {
+module.exports.touch = touchName => {
   // code to implement touch terminal command here
-  fs.writeFile(`${name}`);
+  fs.writeFile(`${touchName}`, '', err => {
+    if (err) throw err;
+    console.log('New file successfully created.');
+    process.exit(0);
+  });
 };
 
-module.exports.mkdir = () => {
+module.exports.mkdir = dirName => {
   // code to implement mkdir terminal command here
-  console.log('mkdir called');
+  fs.mkdir(`./${dirName}`, function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('New directory successfully created.');
+      process.exit(0);
+    }
+  });
 };
